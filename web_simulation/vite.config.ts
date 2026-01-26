d09170f0f051f6c7045e0d88b89497f0b039de4d
Kevin Helders <kevinhelders@yahoo.de>
2026-01-26 21:31:43 +0100

    delete dist

--- web_simulation/vite.config.ts
+++ web_simulation/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "https://naru-kami.github.io/xy-model/",
   build: {
     chunkSizeWarningLimit: 8192,
   },
