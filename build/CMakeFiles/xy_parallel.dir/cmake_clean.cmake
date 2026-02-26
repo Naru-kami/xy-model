3bd5a0b41b4bae3dfccdecdfea3aeab74924f529
CedricLrs <huebbel.ced@gmail.com>
2026-02-26 15:55:25 +0100

    Fix XYModel threading + HDF5 output

---
+++ build/CMakeFiles/xy_parallel.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/xy_parallel.dir/src/main_multithreading.cpp.o"
+  "CMakeFiles/xy_parallel.dir/src/main_multithreading.cpp.o.d"
+  "xy_parallel"
+  "xy_parallel.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/xy_parallel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
